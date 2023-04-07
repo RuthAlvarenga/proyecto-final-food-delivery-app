f335636498ff0a3875b7e0d6a4934639fb5efdd4
Ruth Alvarenga <vanessapalmerola18@gmail.com>
2023-04-07 13:12:35 -0400

    add de fontfamily para navbar

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -5,7 +5,7 @@ import Badge from 'react-bootstrap/Badge';
 import hamburger from '../navbar/hamburger.png';
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import { logoutUsuario } from '../../actions/UsuarioActions';
-
+import '../navbar/Nav.css'
 
 const NavBar = () => {
 
@@ -18,8 +18,8 @@ const NavBar = () => {
         <div>
             <nav style={{ backgroundColor: "#fec800" }} className="navbar navbar-expand-lg mb-4" >
                 <div>
-                    <img src={hamburger} className='img mx-3 mb-2' style={{ width: "40px", height: "40px" }} />
-                    <a className="navbar-brand text-black mx-2 fs-3" href="/">Delicius Hamburger</a>
+                    <img src={hamburger} className='img mx-3 mb-2' style={{width: "50px", height: "50px"}} />
+                    <a className="navbar-brand text-black mx-2 " style={{fontFamily: 'navfont', fontSize: '45px'}} href="/">Delicius Hamburger</a>
                 </div>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,11 +27,11 @@ const NavBar = () => {
                 </button>
                 <div className="collapse navbar-collapse mx-2" id="navbarNav">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
-                        <li className="nav-item">
+                        <li className="nav-item fs-4">
                             <Link className="nav-link text-black" to={'/'}>Home </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link text-black" to={'/orden'}>
+                            <Link className="nav-link text-black fs-4" to={'/orden'}>
                                 Orden
                                 {
                                     ordenState.ordenItems.length > 0 && (
@@ -44,7 +44,7 @@ const NavBar = () => {
                             </Link>
                         </li>
                         {currentUser ?
-                            (<NavDropdown title={currentUser.name} id='basic-nav-dropdown' >
+                            (<NavDropdown className='fs-4 fw-bold' title={currentUser.name} id='basic-nav-dropdown' >
                                     
                             <NavDropdown.Item ><Link style={{textDecoration:'none', color: 'black'}}to={'/pedidos'}>Ordenes</Link></NavDropdown.Item>
                             <NavDropdown.Divider />
@@ -55,7 +55,7 @@ const NavBar = () => {
                             </NavDropdown>
                             )
                             :
-                            <li className="nav-item">
+                            <li className="nav-item fs-4">
 
                                 <Link className="nav-link text-black" to={'/login'}>Login</Link>
                             </li>
